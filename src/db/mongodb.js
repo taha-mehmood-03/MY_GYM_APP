624e0cea1ee71fa53675523d105fc5f76341b088
Taha Mehmood <mehmoodtaha854@gmail.com>
2024-11-15 23:50:37 -0800

    1111111

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,30 +1,22 @@
 import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
 
-dotenv.config();  // Load environment variables from .env.local
-
-// Get the Mongo URI from the environment variables
 const uri = process.env.MONGO_URI;
-const options = {};
-
 let client;
 let clientPromise;
 
-if (!uri) {
+if (!process.env.MONGO_URI) {
   throw new Error('Please add your Mongo URI to .env.local');
 }
 
 if (process.env.NODE_ENV === 'development') {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+    client = new MongoClient(uri);
     global._mongoClientPromise = client.connect();
-    console.log("connected to mongodb")
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
+  client = new MongoClient(uri);
   clientPromise = client.connect();
-  console.log("connected to mongodb")
 }
 
 export default clientPromise;
