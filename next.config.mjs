4daab17210ecea63143a0c7d2ffa56ba4172b162
Taha Mehmood <mehmoodtaha854@gmail.com>
2024-11-16 01:05:56 -0800

    for vercel app1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'), // Adjust based on your structure
+    };
+    return config;
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
