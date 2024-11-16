d2a36f06192167c5e8d603e121d3e98e116e3d75
Taha Mehmood <mehmoodtaha854@gmail.com>
2024-11-15 23:00:05 -0800

    added server.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', 
-  basePath: '',
-  publicDir: 'public',
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://gym4fit-e5eggddufngjajb8.eastus-01.azurewebsites.net/:path*',
-      },
-    ];
-  },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NODE_ENV === 'production' 
-      ? 'https://gym4fit-e5eggddufngjajb8.eastus-01.azurewebsites.net'
-      : 'http://localhost:3000',
-  }
-}
+  output:'standalone',
+};
 
-export default nextConfig
+export default nextConfig;
