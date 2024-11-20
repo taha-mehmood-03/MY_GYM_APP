c3a806ee5dfdd575ee109466d084cb3f26658cad
Taha Mehmood <mehmoodtaha854@gmail.com>
2024-11-20 08:38:32 -0800

    chnages

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -23,7 +23,7 @@ const QUERY_KEYS = {
 const fetchImages = async () => {
   console.log("Fetching images...");
   try {
-    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/gettingImages`);
+    const response = await axios.get(process.env.NEXT_PUBLIC_API_URL+ '/api/auth/gettingImages');
     console.log("Fetched images response:", response.data);
     return response.data;
   } catch (error) {
